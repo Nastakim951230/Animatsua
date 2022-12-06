099f25267e056fc7d6cdd92206e5fcc570ea63bd
nasty <nastytrifono03@gmail.com>
2022-12-06 21:07:00 +0300

    Zadanie 1 Zadanie 2 Zadanie 3

--- Анимация/MainWindow.xaml.cs
+++ Анимация/MainWindow.xaml.cs
@@ -24,21 +24,81 @@ namespace Анимация
         public MainWindow()
         {
             InitializeComponent();
-            //DoubleAnimation WA = new DoubleAnimation();
-            //WA.From = 200;
-            //WA.To = 100;
-            //WA.Duration = TimeSpan.FromSeconds(2);
-            //WA.RepeatBehavior = RepeatBehavior.Forever;
-            //WA.AutoReverse = true;
-            //Reklama.BeginAnimation(WidthProperty, WA);
+            DoubleAnimation WA = new DoubleAnimation();
+            WA.From = 200;
+            WA.To = 100;
+            WA.Duration = TimeSpan.FromSeconds(2);
+            WA.RepeatBehavior = RepeatBehavior.Forever;
+            WA.AutoReverse = true;
+            Reklama.BeginAnimation(WidthProperty, WA);
 
-            //DoubleAnimation HA = new DoubleAnimation();
-            //HA.From = 50;
-            //HA.To = 40;
-            //HA.Duration = TimeSpan.FromSeconds(2);
-            //HA.RepeatBehavior = RepeatBehavior.Forever;
-            //HA.AutoReverse = true;
-            //Reklama.BeginAnimation(HeightProperty, HA);
+
+            DoubleAnimation HA = new DoubleAnimation();
+            HA.From = 30;
+            HA.To = 40;
+            HA.Duration = TimeSpan.FromSeconds(2);
+            HA.RepeatBehavior = RepeatBehavior.Forever;
+            HA.AutoReverse = true;
+            Reklama.BeginAnimation(HeightProperty, HA);
+
+            ColorAnimation colorbtn1 = new ColorAnimation();
+            ColorConverter colorConverterbtn1 = new ColorConverter();
+            Color ColorStartbtn1 = (Color)colorConverterbtn1.ConvertFrom("#ff6274");
+            Reklama.BorderBrush = new SolidColorBrush(ColorStartbtn1);
+            colorbtn1.From = ColorStartbtn1;
+            colorbtn1.To = (Color)colorConverterbtn1.ConvertFrom("#743eff");
+            colorbtn1.Duration = TimeSpan.FromSeconds(2);
+            colorbtn1.RepeatBehavior = RepeatBehavior.Forever;
+            colorbtn1.AutoReverse = true;
+            Reklama.BorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, colorbtn1);
+
+            ColorAnimation colorbtn = new ColorAnimation();
+            ColorConverter colorConverterbtn = new ColorConverter();
+            Color ColorStartbtn = (Color)colorConverterbtn.ConvertFrom("#fdd4d8");
+            Reklama.Background = new SolidColorBrush(ColorStartbtn);
+            colorbtn.From = ColorStartbtn;
+            colorbtn.To = (Color)colorConverterbtn.ConvertFrom("#c2cbfc");
+            colorbtn.Duration = TimeSpan.FromSeconds(2);
+            colorbtn.RepeatBehavior = RepeatBehavior.Forever;
+            colorbtn.AutoReverse = true;
+            Reklama.Background.BeginAnimation(SolidColorBrush.ColorProperty, colorbtn);
+
+
+            ColorAnimation color =new ColorAnimation();
+            ColorConverter colorConverter=new ColorConverter();
+            Color ColorStart = (Color)colorConverter.ConvertFrom("#ff0000");
+            aktsia.Foreground = new SolidColorBrush(ColorStart);
+            color.From=ColorStart;
+            color.To=(Color)colorConverter.ConvertFrom("#a80ced");
+            color.Duration = TimeSpan.FromSeconds(2);
+            color.RepeatBehavior = RepeatBehavior.Forever;
+            color.AutoReverse = true;
+            aktsia.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, color);
+
+            DoubleAnimation size = new DoubleAnimation();
+            size.From = 18;
+            size.To = 24;
+            size.Duration = TimeSpan.FromSeconds(2);
+            size.RepeatBehavior = RepeatBehavior.Forever;
+            size.AutoReverse = true;
+            aktsia.BeginAnimation(FontSizeProperty, size);
+        }
+
+        private void Reklama_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation WA = new DoubleAnimation();
+            WA.From = 250;
+            WA.To = 270;
+            WA.Duration = TimeSpan.FromSeconds(2);
+          
+            img.BeginAnimation(WidthProperty, WA);
+
+            DoubleAnimation HA = new DoubleAnimation();
+            HA.From = 200;
+            HA.To = 220;
+            HA.Duration = TimeSpan.FromSeconds(2);
+            img.BeginAnimation(HeightProperty, HA);
+
         }
     }
 }
